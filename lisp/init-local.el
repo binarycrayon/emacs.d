1a77d0a91bb574a8a7d6e3f75c6dd4c65037dbf1
binarycrayon <binarycrayon@gmail.com>
2017-06-13 17:23:51 -0700

    [master] Add and enable linum-relative

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,7 +1,15 @@
-;;; init-local --- custom init
+;;; init-local --- my custom init
+;;; Commentary:
+;;; this is my custom init
 
 ;;; Code:
 (global-linum-mode t)
+
+(when (maybe-require-package 'linum-relative)
+  (add-hook 'after-init-hook 'linum-relative-mode))
+
+
+(provide 'init-linenum-relative)
 
 (provide 'init-local)
 ;;; init-local ends here
